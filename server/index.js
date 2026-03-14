72f1c6cb6e1f42c31a878fcf874b1ecb241a0b34
Sashi2809 <sashidhar.k007@gmail.com>
2026-03-14 16:51:49 +0530

    updated backend url to render

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173", 
-  "https://lms-deploy-project-98fo.vercel.app/"
+  "https://lms-deploy-project-98fo.vercel.app"
 ];
 
 app.use(
